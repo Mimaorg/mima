0b9d173f4f634684cdcde43ff4ede38bf53b1e3a
unknown <Mimaorg>
2020-12-27 01:06:51 +0500

    multi cheque

---
+++ memaNew_december_27_2020/WebApplication1/WebApplication1/Content/UsersController.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Content
+{
+    public class Users1Controller : Controller
+    {
+        // GET: Users
+
+        Mima_Finance_Entities db = new Mima_Finance_Entities();
+        public ActionResult allUsers()
+        {
+            return View(db.Users.ToList());
+        }
+
+        public JsonResult getDatabyid(int id)
+        {
+            var a = db.Users.Find(id);
+            var ToJson = JsonConvert.SerializeObject(db.Users.Find(id));
+            return Json(ToJson, JsonRequestBehavior.AllowGet);
+            
+
+        }
+
+
+        public ActionResult addEditUsers(User formdata)
+        {
+            if (formdata.User_ID == 0)
+            {
+                db.Users.Add(formdata);
+                db.SaveChanges();
+            }
+            else
+            {
+
+
+            }
+           
+
+            return PartialView("userList",db.Users.ToList());
+
+        }
+    }
+}
